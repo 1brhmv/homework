caaee508274a5c1d2bbc6ffe47400d7dfdf72e2b
1brhmv <108865357+1brhmv@users.noreply.github.com>
2023-03-01 01:24:35 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,20 @@
+//task1
+/*
+
+var num = [3, 6, 9, 12, 15, 18, 21, 24];
+
+
+for (let i = 0; i < num.length; i++) {
+  if (num,[i] % 3 === 0) {
+    console.log(num[i]);
+  }
+}
+*/
+//task2
+var num = [3, 6, 1];
+
+for (let i = 0 ; i < num.length;i++){
+    if (num[i]> 2 )
+   
+    console.log(num[i]);
+}
