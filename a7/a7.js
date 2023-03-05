0ae3b712ef784a3d09bfa290b5259b3cde44fcea
1brhmv <108865357+1brhmv@users.noreply.github.com>
2023-03-06 01:15:14 +0400

    Add files via upload
    i did only task 1 . didnt understand resize method

---
+++ a7/a7.js
@@ -0,0 +1,21 @@
+/*
+const finCodes = ["FIN123", "FIN456", "FIN789"];
+const finCodePattern = /^FIN\d{3}$/;
+
+let finCode;
+do {
+  finCode = prompt("Enter the finCode:");
+} while (!finCodePattern.test(finCode));
+
+
+const i = finCodes.indexOf(finCode);
+if (i=== -1) {
+  alert("finCode not found");
+} else {
+  alert("User found!");
+  console.log(`Index of {finCode} is {i}`);
+}
+*/
+const   Array[];
+const 
+
